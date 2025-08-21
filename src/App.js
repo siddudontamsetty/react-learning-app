5a311ba2b6e0dc449f45890358c015b529031f48
siddudontamsetty <siddudontamsetty@gmail.com>
2025-08-21 22:19:48 +0530

    initiated routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route , Link} from "react-router-dom";
+import { Home } from "./pages/Home"; 
+import { Menu } from "./pages/Menu";
+import { Contact } from "./pages/Contact";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        
+          <h1> <Link to="/">home Page</Link> </h1>
+          <h1> <Link to = "/menu">menu Page</Link> </h1>
+          <h1> <Link to ="/contact">contact Page</Link> </h1>
+        
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<h1>404 Not Found</h1>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
