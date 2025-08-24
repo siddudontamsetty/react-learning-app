01a29e68341631d062e594d3ceea658471d9e278
siddudontamsetty <siddudontamsetty@gmail.com>
2025-08-24 19:58:10 +0530

    useContext hook

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route , Link} from "react-router-dom";
-import { Home } from "./pages/Home"; 
-import { Menu } from "./pages/Menu";
-import { Contact } from "./pages/Contact";
+import { createContext,userContext } from "react";
 
+const myContext = createContext ();
 function App() {
   return (
-    <div className="App">
-      <Router>
-        
-          <h1> <Link to="/">home Page</Link> </h1>
-          <h1> <Link to = "/menu">menu Page</Link> </h1>
-          <h1> <Link to ="/contact">contact Page</Link> </h1>
-        
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<h1>404 Not Found</h1>} />
-        </Routes>
-      </Router>
-    </div>
+    <myContext.Provider value ="sidhartha">
+      <UserName/>
+    </myContext.Provider>
   );
 }
+
+const UserName =()=>{
+  const user = useContext(myContext)
+  return (<h1>username is {user}</h1>)
+}
+
 
 export default App;
