0ebe48e628ce1e91f4873b4ace49ca0aff2dc11d
siddudontamsetty <siddudontamsetty@gmail.com>
2025-08-24 20:13:13 +0530

    context with multiple values

--- src/App.js
+++ src/App.js
@@ -4,17 +4,27 @@ import { createContext,userContext } from "react";
 
 const myContext = createContext ();
 function App() {
+  const data ={
+    name : "siddu" ,
+    age :21 ,
+    gender :"male"
+  }
   return (
-    <myContext.Provider value ="sidhartha">
-      <UserName/>
+    <myContext.Provider value = {data}>
+      <UserData/>
     </myContext.Provider>
   );
 }
 
-const UserName =()=>{
-  const user = useContext(myContext)
-  return (<h1>username is {user}</h1>)
+const UserData=()=>{
+  const {name , age , gender} = useContext(myContext)
+  return ( <div>personal info is
+    <h1>name : {name}</h1>
+    <h1>age :{age}</h1>
+    <h1>gender:{gender}</h1>
+  </div>)
 }
+
 
 
 export default App;
